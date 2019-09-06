977a51780d855409088e829487578bb9b4b19b92
stephen <stephen@gethappie.me>
2019-09-06 11:47:22 -0400

    Set up App and components directory

---
+++ src/Components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './dashboard.css';
+
+function Dashboard () {
+  return (
+    <div className="dashboard">
+      <div>Hello Anand</div>
+    </div>
+  )
+}
+
+export default Dashboard
